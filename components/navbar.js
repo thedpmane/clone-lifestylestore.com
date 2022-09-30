47369d91c1217beec7230400e0666eac64a3a491
Navneet <navneetburman900@gmail.com>
2022-09-30 18:08:44 +0530

    added this file

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,33 @@
 function navbar(){
-    return ``
+    return `
+    <div id="navbar1">
+    <div id="a">
+        <img src="" alt="">
+        <h4>Free shipping</h4>
+        <img src="" alt="">
+        <h4>Click & Connect</h4>
+        <img src="" alt="">
+        <h4>Return To Store</h4>
+    </div>
+    <div id="b">
+    <h4>Download Our Apps |</h4>
+    <h4>Store Locator |</h4>
+    <h4>Help</h4>
+    </div>
+</div>
+<div id="navbar2">
+    <img src="https://i1.lmsin.net/website_images/in/logos/logo-lifestyle.svg" alt="">
+    <button>Women</button>
+    <button>Mens</button>
+    <button>Kids</button>
+    <button>Shoes&Bag</button>
+    <button>Beauty</button>
+    <input type="search" placeholder="What are you looking for?">
+    <p>More |</p>
+    <p>Sign up/Sign In</p>
+</div>
+<div id="navbar3">
+    <h4>Buy 1 Get 1 Free on Best of our Brands></h4>
+</div>`
 }
+export  {navbar};
