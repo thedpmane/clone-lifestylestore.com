e707a93049c67276221adfccc04967cdc955f7ce
Mr-Sharma17 <as.ashishsharma980@gmail.com>
2022-10-01 12:23:59 +0530

    done some modifications

--- scripts/women page.js
+++ scripts/women page.js
@@ -16,10 +16,16 @@ const SlideShowData = [
         img: 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktop-3-26Sep2022.gif',
     },
     {
+        img : 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktop-3-29Sep2022.gif',
+    },
+    {
         img: 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-2-26Sep2022.jpg',
     },
     {
         img: 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-4-26Sep2022.jpg',
+    },
+    {
+        img : 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner5-desktop-Women-29Sep2022.jpg',
     },
     {
         img: 'https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner5-desktop-Women-26Sep2022.jpg',
